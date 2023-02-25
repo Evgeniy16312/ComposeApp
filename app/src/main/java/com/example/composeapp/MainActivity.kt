e77654139a50d81621f99ae11c394b0db10ba91d
Evgeniy <etyurin@axbit.ru>
2023-02-25 12:23:45 +0400

    start compose lesson one

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -1,40 +1,88 @@
 package com.example.composeapp
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Message
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setContent {
-            MessageCard(Message("android","jetpack compose"))
+            MessageCard(Message("arnold", "schwartz"))
         }
     }
 
     data class Message(
-        val author : String,
-        val body : String
+        val author: String,
+        val body: String
     )
 
     @Composable
-    fun MessageCard(msg : Message) {
-        Column {
-            Text(text = msg.author)
-            Text(text = msg.body)
+    fun MessageCard(msg: Message) {
+        Row(modifier = Modifier.padding(all = 8.dp)) {
+            Image(
+                painter = painterResource(R.drawable.arni),
+                contentDescription = null,
+                modifier = Modifier
+                    // Set image size to 40 dp
+                    .size(40.dp)
+                    // Clip image to be shaped as a circle
+                    .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colors.primary, CircleShape)
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column {
+                Text(
+                    text = msg.author,
+                    color = MaterialTheme.colors.secondaryVariant,
+                    style = MaterialTheme.typography.subtitle1
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Surface(shape = MaterialTheme.shapes.small, elevation = 1.dp) {
+                    Text(
+                        text = msg.body,
+                        Modifier.padding(4.dp),
+                        color = MaterialTheme.colors.secondaryVariant,
+                        style = MaterialTheme.typography.subtitle2
+                    )
+                }
+            }
         }
     }
 
-    @Preview
+    @Preview(name = "Light Mode")
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true,
+        name = "Dark Mode"
+    )
     @Composable
     fun PreviewMessageCard() {
-        MessageCard(msg =
-      Message("Android","jetpack compose"))
+        MaterialTheme {
+            Surface {
+                MessageCard(
+                    msg =
+                    Message("Arnold", "schwartz")
+                )
+            }
+        }
     }
 }
